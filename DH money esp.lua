9da1de1b5f1c70d27642a0eed70443663dd38446
Margus Kuusk <106272829+Muqa1@users.noreply.github.com>
2023-08-17 04:41:05 +0300

    Update DH money esp.lua

--- DH money esp.lua
+++ DH money esp.lua
@@ -1,26 +1,26 @@
 game:GetService("RunService").RenderStepped:Connect(function()
-	for i, m in pairs(game:GetService("Workspace").Ignored.Drop:GetChildren()) do 
-		if m.Name == "MoneyDrop" then 
-			if not m:FindFirstChild("ASDWAgFWE2") then
-				local billboard = Instance.new("BillboardGui")
-    				billboard.Name = "ASDWAgFWE2"
-    				billboard.AlwaysOnTop = true
-    				billboard.Size = UDim2.new(4, 0, 4, 0) 
-    				billboard.Adornee = m
-    				billboard.Parent = m	
+    for i, m in pairs(game:GetService("Workspace").Ignored.Drop:GetChildren()) do 
+        if m.Name == "MoneyDrop" then 
+            if not m:FindFirstChild("ASDWAgFWE2") then
+	        local billboard = Instance.new("BillboardGui")
+    		billboard.Name = "ASDWAgFWE2"
+    		billboard.AlwaysOnTop = true
+    		billboard.Size = UDim2.new(4, 0, 4, 0) 
+    		billboard.Adornee = m
+    		billboard.Parent = m	
     	
-    				local nameLabel = Instance.new("TextLabel")
-        			nameLabel.BackgroundTransparency = 1
-        			nameLabel.Text = "Money"
-        			nameLabel.TextScaled = true
-        			nameLabel.TextWrapped = true
-        			nameLabel.Font = Enum.Font.Code
-        			nameLabel.TextColor3 = Color3.new(1, 1, 1)
-        			nameLabel.TextSize = 20
-        			nameLabel.Position = UDim2.new(0, 0, 0, 0)
-        			nameLabel.Size = UDim2.new(1, 0, 1, 0)
-        			nameLabel.Parent = billboard
-			end
-		end
+    		local nameLabel = Instance.new("TextLabel")
+        	nameLabel.BackgroundTransparency = 1
+        	nameLabel.Text = "Money"
+        	nameLabel.TextScaled = true
+        	nameLabel.TextWrapped = true
+        	nameLabel.Font = Enum.Font.Code
+        	nameLabel.TextColor3 = Color3.new(1, 1, 1)
+        	nameLabel.TextSize = 20
+        	nameLabel.Position = UDim2.new(0, 0, 0, 0)
+        	nameLabel.Size = UDim2.new(1, 0, 1, 0)
+        	nameLabel.Parent = billboard
+	    end
 	end
+    end
 end)
