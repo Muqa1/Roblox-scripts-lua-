458bd93c09033016a3361a11eefbcb580fdeb4f5
Margus Kuusk <106272829+Muqa1@users.noreply.github.com>
2023-08-17 04:32:31 +0300

    Update first esp.lua

--- first esp.lua
+++ first esp.lua
@@ -2,6 +2,7 @@ local health_bar = true
 local box = true
 local name = true
 local team_color = true
+local enemy_only = true
 
 local player = game.Players.LocalPlayer
 local players = {} 
@@ -10,25 +11,27 @@ local core = game:GetService("CoreGui")
 local function CreateBillboard(adornee, playerName, playerHealth, playerMaxHealth, playerTeamColor)
     local billboard = Instance.new("BillboardGui")
     billboard.Name = "E"
-    
     billboard.AlwaysOnTop = true
     billboard.Size = UDim2.new(4, 0, 6.5, 0) 
     billboard.StudsOffset = Vector3.new(0, -0.5, 0)
     billboard.Adornee = adornee
-
     billboard.Parent = core
     
     if box then
         local frame = Instance.new("Frame")
-        if team_color then
-            frame.BackgroundColor3 = playerTeamColor.Color
-        else
-            frame.BackgroundColor3 = Color3.new(0, 0, 0)
-        end
-        frame.BackgroundTransparency = 0.35
+        frame.BackgroundTransparency =  1
         frame.BorderSizePixel = 1
-        frame.BorderColor3 = Color3.new(1, 1, 1)
         frame.Size = UDim2.new(1, 0, 1, 0)
+        
+        local UiStroke = Instance.new("UIStroke")
+        UiStroke.Enabled = true
+        UiStroke.Thickness = 1
+        if team_color then
+            UiStroke.Color = playerTeamColor.Color
+        else
+            UiStroke.Color = Color3.new(1,1,1)
+        end
+        UiStroke.Parent = frame
 
         frame.Parent = billboard
     end
@@ -38,7 +41,6 @@ local function CreateBillboard(adornee, playerName, playerHealth, playerMaxHealt
         healthBar.BackgroundColor3 = Color3.new(0, 1, 0) 
         healthBar.BorderSizePixel = 1
         healthBar.Size = UDim2.new(0, 2, playerHealth / playerMaxHealth, 1)
-
         healthBar.Parent = billboard
     end
 
@@ -49,21 +51,25 @@ local function CreateBillboard(adornee, playerName, playerHealth, playerMaxHealt
         nameLabel.Font = Enum.Font.Code
         nameLabel.TextColor3 = Color3.new(1, 1, 1)
         nameLabel.TextSize = 12
-        nameLabel.Position = UDim2.new(0, 0, 0, 0)
-        nameLabel.Size = UDim2.new(1, 0, 0.1, 0)
+        nameLabel.Position = UDim2.new(0, 0, -0.5, 0)
+        nameLabel.Size = UDim2.new(1, 0, 1, 0)
+        
+        local UiStroke = Instance.new("UIStroke")
+        UiStroke.Enabled = true
+        UiStroke.Thickness = 0.5
+        UiStroke.Color = Color3.new(0,0,0)
+        UiStroke.Parent = nameLabel
 
         nameLabel.Parent = billboard
     end
 end
 
-
 local function GetEnemyPlayers()
     players = {}
     if #game:GetService("Teams"):GetTeams() > 0 then
-    local friendly = player.Team.Name
-    for i,v in pairs(game:GetService("Teams"):GetTeams()) do
-        if v.Name ~= friendly and v.Name ~= (game.Teams:FindFirstChild("Spectators") and game.Teams.Spectators.Name) then
-            print("Enemy team: "..v.Name)
+        local friendly = player.Team.Name
+        for i,v in pairs(game:GetService("Teams"):GetTeams()) do
+            if v.Name ~= friendly and v.Name ~= (game.Teams:FindFirstChild("Spectators") and game.Teams.Spectators.Name) then
                 local enemyPlayers = v:GetPlayers()
                 for i,v in pairs(enemyPlayers) do
                     table.insert(players, v)
@@ -80,7 +86,12 @@ local function InsertBillboardToPlayers()
             v:Destroy()
         end
     end
-    local allPlayers = GetEnemyPlayers() -- game.Players:GetPlayers()
+    local allPlayers
+    if enemy_only then 
+    	allPlayers = GetEnemyPlayers()
+    else
+    	allPlayers = game.Players:GetPlayers()
+    end
     for i,v in pairs(allPlayers) do
         local health = v.Character and v.Character:FindFirstChild("Humanoid") and v.Character.Humanoid.Health or 0
         local maxHealth = v.Character and v.Character:FindFirstChild("Humanoid") and v.Character.Humanoid.MaxHealth or 1
@@ -91,11 +102,11 @@ local function InsertBillboardToPlayers()
     end
 end
 
-player:GetMouse().KeyDown:Connect(function(key)
-    if key:lower() == "k" then
-        InsertBillboardToPlayers()
-    end
-end)
+InsertBillboardToPlayers()
+
+while wait(1) do 
+    InsertBillboardToPlayers()
+end
 
 game.Players.PlayerAdded:Connect(function(plr)
     plr.CharacterAdded:Wait()
@@ -106,9 +117,3 @@ game.Players.PlayerRemoving:Connect(function(plr)
     plr.CharacterRemoving:Wait()
     InsertBillboardToPlayers()
 end)
-
-local function UpdateBillboards()
-    InsertBillboardToPlayers()
-end
-
-game:GetService("RunService").Heartbeat:Connect(UpdateBillboards)
